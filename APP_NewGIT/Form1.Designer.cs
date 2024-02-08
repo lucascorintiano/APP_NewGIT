7e3faa183e8c716f3967d01ab1c4c99cc639eb60
lucascorintiano <lucassantosgregorio@gmail.com>
2024-02-08 16:21:11 -0300

    segunda tela

--- APP_NewGIT/Form1.Designer.cs
+++ APP_NewGIT/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "GIT";
+            this.Text = "git ";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
